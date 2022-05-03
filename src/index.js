89b5ec6fe31e0c7fe243bc81547e221dc4b2d4b8
Joe Fleming <github@joefleming.net>
2022-05-03 17:11:32 -0600

    chore: minor refactor, create write function

--- src/index.js
+++ src/index.js
@@ -5,30 +5,35 @@ const { gqlPluckFromCodeString } = require('@graphql-tools/graphql-tag-pluck');
 
 async function getContents() {
   const source = core.getInput('source');
-  const filepath = path.resolve(__dirname, `../${source}`);
-
   core.debug(`Reading from file ${source}`);
 
+  const filepath = path.resolve(__dirname, `../${source}`);
   return fs.readFile(filepath, 'utf8');
+}
+
+async function writeContents(content) {
+  const output = core.getInput('output');
+  core.info(`Writing to file ${output}`);
+
+  const filepath = path.resolve(__dirname, `../${output}`);
+  await fs.writeFile(filepath, content);
+  return filepath;
 }
 
 async function main() {
   const source = core.getInput('source');
-  const output = core.getInput('output');
 
   core.debug(`Reading from ${source}`);
 
   const content = await getContents();
-
-  core.info(`Created file at ${output}`);
-  core.setOutput('filepath', output);
 
   const [plucked] = await gqlPluckFromCodeString(
     source, // this parameter is required to detect file type
     content
   );
 
-  await fs.writeFile(output, plucked.body);
+  const output = await writeContents(plucked.body);
+  core.setOutput('filepath', output);
 }
 
 main().catch((error) => {
