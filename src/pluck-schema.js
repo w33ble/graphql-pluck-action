ee95082c778f4eefcddd989bdefeea1fbe2467bd
chestercharles <chestercarmer@icloud.com>
2022-08-11 22:19:37 -0700

    chore: remove jsdoc

--- src/pluck-schema.js
+++ src/pluck-schema.js
@@ -5,10 +5,6 @@ const { glob } = require('glob');
 const { asyncMap: map, asyncFilter: filter, asyncFlow: flow } = require('fp-async-utils');
 const { print } = require('graphql');
 
-/**
- * @param {string} source
- * @returns {Promise<string[]>}
- */
 async function getFilepaths(source) {
   return new Promise((resolve, reject) => {
     glob(source, { nonull: true }, (err, filePaths) => {
@@ -21,38 +17,20 @@ async function getFilepaths(source) {
   });
 }
 
-/**
- * @param {string} filePath
- * @returns {Promise<{filePath: string, content: string}>}
- */
 async function getContent(filePath) {
   const content = await fs.readFile(filePath, 'utf8');
   return { filePath, content };
 }
 
-/**
- *
- * @param {{filePath: string, content: string}} param0
- * @returns {Promise<string>}
- */
 async function pluckGQL({ filePath, content }) {
   const [plucked] = await gqlPluckFromCodeString(filePath, content);
   return plucked && plucked.body;
 }
 
-/**
- * @param {string[]} schemas
- * @returns {Promise<string>}
- */
 async function mergeGql(schemas) {
   return mergeTypeDefs(schemas);
 }
 
-/**
- *
- * @param {DocumentNode} schemaDocumentNode
- * @returns {Promise<string>}
- */
 async function extractSchemaString(schemaDocumentNode) {
   return print(schemaDocumentNode);
 }
